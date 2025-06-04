a7f230d7aa5db917a7be78d6f2c0aa7b70a5471f
S311319 <s311319@studenti.polito.it>
2025-06-04 18:16:27 +0200

    grafo

--- database/DAO.py
+++ database/DAO.py
@@ -41,13 +41,18 @@ class DAO():
     def getEdges(self, storeId, numMin):
         conn = DBConnect.get_connection()
         cursor = conn.cursor(dictionary=True)
-        query = """select distinctrow o.order_id, o2.order_id, o.order_date, o2.order_date, (oi.quantity + oi2.quantity) as qtyOrder
+        query = """with tot_quantity as (select oi.order_id, sum(oi.quantity) as tot
+                    from order_items oi 
+                    group by oi.order_id)
+                    
+                    select distinctrow o.order_id, o2.order_id, o.order_date, o2.order_date, (tot_quantity.tot + oi2.tot_quantity.tot) as qtyOrder
                     from orders o, orders o2, order_items oi, order_items oi2 
                     where o.order_id = oi.order_id and o2.order_id = oi2.order_id 
                     and o.order_id != o2.order_id 
                     and o.order_date < o2.order_date
-                    and o2.store_id = %s
-                    and datediff(o2.order_date, o.order_date) <= %s """
+                    and o2.store_id = 1
+                    and datediff(o2.order_date, o.order_date) <= 5 """
+
         cursor.execute(query, (storeId, numMin, ))
 
         res = []
